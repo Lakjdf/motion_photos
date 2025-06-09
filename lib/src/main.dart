71b7e5cdc2b3dd467b6f3941b619acbfc70d5437
Lakjdf <13982482+Lakjdf@users.noreply.github.com>
2025-06-09 12:06:58 +0200

    switch to in-memory option only

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:motion_photos/src/boyermoore_search.dart';
@@ -9,100 +8,32 @@ import 'package:motion_photos/src/video_index.dart';
 /// This class is responsible for classifying a file as motion photo,
 /// if so, extracts the [VideoIndex] from the motion photo.
 /// extracts the VideoContent of the motion photo.
-abstract class MotionPhotos {
-  Future<void> loadBuffer();
+class MotionPhotos {
+  final Uint8List _buffer;
+
+  MotionPhotos(Uint8List buffer) : _buffer = buffer;
 
   /// isMotionPhoto returns true if the file is a motion photo
-  Future<bool> isMotionPhoto();
+  bool isMotionPhoto() => getMotionVideoIndex() != null;
 
   /// getMotionVideoIndex returns the [VideoIndex] of the photo if it's a motion photo
   /// otherwise returns null
-  Future<VideoIndex?> getMotionVideoIndex();
+  VideoIndex? getMotionVideoIndex() {
+    // Note: The order of the following methods is important
+    // We need to check for MP4 header, then XMP.
+    final int mp4Index = boyerMooreSearch(_buffer, MotionPhotoConstants.mp4HeaderPattern);
+    if (mp4Index != -1) {
+      return VideoIndex(start: mp4Index, end: _buffer.lengthInBytes);
+    }
+    return MotionPhotoHelpers.extractVideoIndexFromXMP(_buffer);
+  }
 
   /// getMotionVideo returns the video portion of the motion photo.
   /// If [index] is not provided, it will be extracted.
-  Future<Uint8List> getMotionVideo({VideoIndex? index});
-
-  /// getMotionVideoFile returns the video portion of the motion photo as a file.
-  /// If [index] is not provided, it will be extracted.
-  /// [destDirectory] is the directory where the file will be saved.
-  /// [fileName] is the name of the video file that will be created in the [destDirectory].
-  Future<File> getMotionVideoFile(
-    Directory destDirectory, {
-    String fileName = 'motionphoto.mp4',
-    VideoIndex? index,
-  });
-}
-
-mixin BaseMotionPhotos implements MotionPhotos {
-  late Uint8List buffer;
-
-  @override
-  Future<bool> isMotionPhoto() async {
-    try {
-      await loadBuffer();
-      return (await getMotionVideoIndex()) != null;
-    } catch (e) {
-      return false;
-    }
+  /// returns null if it's not a motion photo.
+  Uint8List? getMotionVideo({VideoIndex? index}) {
+    final videoIndex = index ?? getMotionVideoIndex();
+    if (videoIndex == null) return null;
+    return _buffer.buffer.asUint8List(videoIndex.start, videoIndex.videoLength);
   }
-
-  @override
-  Future<VideoIndex?> getMotionVideoIndex() async {
-    await loadBuffer();
-    // Note: The order of the following methods is important
-    // We need to check for MP4 header, then XMP.
-    final int mp4Index = boyerMooreSearch(buffer, MotionPhotoConstants.mp4HeaderPattern);
-    if (mp4Index != -1) {
-      return VideoIndex(start: mp4Index, end: buffer.lengthInBytes);
-    }
-    return MotionPhotoHelpers.extractVideoIndexFromXMP(buffer);
-  }
-
-  @override
-  Future<Uint8List> getMotionVideo({VideoIndex? index}) async {
-    final videoIndex = index ?? (await getMotionVideoIndex());
-    if (videoIndex == null) {
-      throw Exception('unable to find video index');
-    }
-    return buffer.buffer.asUint8List(videoIndex.start, videoIndex.videoLength);
-  }
-
-  @override
-  Future<File> getMotionVideoFile(
-    Directory destDirectory, {
-    String fileName = 'motionphoto.mp4',
-    VideoIndex? index,
-  }) async {
-    Directory tempDir = destDirectory;
-    Uint8List videoBuffer = await getMotionVideo(index: index);
-    return File('${tempDir.path}/$fileName').writeAsBytes(
-      videoBuffer,
-    );
-  }
-}
-
-class FileMotionPhotos with BaseMotionPhotos {
-  final String filePath;
-  bool bufferLoaded = false;
-
-  FileMotionPhotos(this.filePath);
-
-  @override
-  Future<void> loadBuffer() async {
-    if (!bufferLoaded) {
-      final File file = File(filePath);
-      buffer = await file.readAsBytes();
-      bufferLoaded = true;
-    }
-  }
-}
-
-class InMemoryMotionPhotos with BaseMotionPhotos {
-  InMemoryMotionPhotos(Uint8List buffer) {
-    this.buffer = buffer;
-  }
-
-  @override
-  Future<void> loadBuffer() async {}
 }
--- test/motion_photos_test.dart
+++ test/motion_photos_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:motion_photos/motion_photos.dart';
 import 'package:motion_photos/src/boyermoore_search.dart';
 import 'package:motion_photos/src/constants.dart';
@@ -5,71 +7,67 @@ import 'package:test/test.dart';
 
 void main() {
   group('isMotionPhoto', () {
-    test('JPEG MotionPhoto', () async {
-      final motionPhotos = FileMotionPhotos('assets/motionphoto.jpg');
-      expect(await motionPhotos.isMotionPhoto(), true);
+    test('JPEG MotionPhoto', () {
+      final motionPhotos = MotionPhotos(File('assets/motionphoto.jpg').readAsBytesSync());
+      expect(motionPhotos.isMotionPhoto(), true);
     });
 
-    test('HEIF MotionPhoto', () async {
-      final motionPhotos = FileMotionPhotos('assets/motionphoto.heic');
-      expect(await motionPhotos.isMotionPhoto(), true);
+    test('HEIF MotionPhoto', () {
+      final motionPhotos = MotionPhotos(File('assets/motionphoto.heic').readAsBytesSync());
+      expect(motionPhotos.isMotionPhoto(), true);
     });
 
     //  https://github.com/ente-io/photos-app/issues/1551
-    test('Pixel 6 Top shot', () async {
-      final motionPhotos = FileMotionPhotos('assets/pixel_6_small_video.jpg');
-      expect(await motionPhotos.isMotionPhoto(), true);
+    test('Pixel 6 Top shot', () {
+      final motionPhotos = MotionPhotos(File('assets/pixel_6_small_video.jpg').readAsBytesSync());
+      expect(motionPhotos.isMotionPhoto(), true);
     });
 
-    test('Not a MotionPhoto', () async {
-      final motionPhotos = FileMotionPhotos('assets/normalphoto.jpg');
-      expect(await motionPhotos.isMotionPhoto(), false);
+    test('Not a MotionPhoto', () {
+      final motionPhotos = MotionPhotos(File('assets/normalphoto.jpg').readAsBytesSync());
+      expect(motionPhotos.isMotionPhoto(), false);
     });
 
-    test('Not a MotionPhoto (Pixel8)', () async {
-      final motionPhotos = FileMotionPhotos('assets/pixel_8.jpg');
-      expect(await motionPhotos.isMotionPhoto(), false);
+    test('Not a MotionPhoto (Pixel8)', () {
+      final motionPhotos = MotionPhotos(File('assets/pixel_8.jpg').readAsBytesSync());
+      expect(motionPhotos.isMotionPhoto(), false);
     });
   });
 
   group('getVideoIndex', () {
-    test('JPEG MotionPhoto', () async {
-      final motionPhotos = FileMotionPhotos('assets/motionphoto.jpg');
-      final actualResult = await motionPhotos.getMotionVideoIndex();
+    test('JPEG MotionPhoto', () {
+      final motionPhotos = MotionPhotos(File('assets/motionphoto.jpg').readAsBytesSync());
+      final actualResult = motionPhotos.getMotionVideoIndex();
       const expectedResult = VideoIndex(start: 3366251, end: 8013982);
       expect(actualResult!, expectedResult);
     });
 
-    test('HEIF MotionPhoto', () async {
-      final motionPhotos = FileMotionPhotos('assets/motionphoto.heic');
-      final actualResult = await motionPhotos.getMotionVideoIndex();
+    test('HEIF MotionPhoto', () {
+      final motionPhotos = MotionPhotos(File('assets/motionphoto.heic').readAsBytesSync());
+      final actualResult = motionPhotos.getMotionVideoIndex();
       const expectedResult = VideoIndex(start: 1455411, end: 3649069);
       expect(actualResult!, expectedResult);
     });
 
-    test('Not a motion photo_pixel', () async {
-      final motionPhotos = FileMotionPhotos('assets/normalphoto.jpg');
-      final actualResult = await motionPhotos.getMotionVideoIndex();
+    test('Not a motion photo_pixel', () {
+      final motionPhotos = MotionPhotos(File('assets/normalphoto.jpg').readAsBytesSync());
+      final actualResult = motionPhotos.getMotionVideoIndex();
       expect(actualResult == null, true);
     });
   });
 
   group('getMotionVideo', () {
-    test('JPEG MotionPhoto', () async {
-      final motionPhotos = FileMotionPhotos('assets/motionphoto.jpg');
-      final videoBuffer = await motionPhotos.getMotionVideo();
-      final hasVideoContent = boyerMooreSearch(
-              videoBuffer, MotionPhotoConstants.mp4HeaderPattern) !=
-          -1;
+    test('JPEG MotionPhoto', () {
+      final motionPhotos = MotionPhotos(File('assets/motionphoto.jpg').readAsBytesSync());
+      final videoBuffer = motionPhotos.getMotionVideo()!;
+      final hasVideoContent = boyerMooreSearch(videoBuffer, MotionPhotoConstants.mp4HeaderPattern) != -1;
       expect(hasVideoContent, true);
     });
 
-    test('HEIF MotionPhoto', () async {
-      final motionPhotos = FileMotionPhotos('assets/motionphoto.heic');
-      final videoBuffer = await motionPhotos.getMotionVideo();
-      final hasVideoContent = boyerMooreSearch(
-              videoBuffer, MotionPhotoConstants.mp4HeaderPattern) !=
-          -1;
+    test('HEIF MotionPhoto', () {
+      final motionPhotos = MotionPhotos(File('assets/motionphoto.heic').readAsBytesSync());
+      final videoBuffer = motionPhotos.getMotionVideo()!;
+      final hasVideoContent = boyerMooreSearch(videoBuffer, MotionPhotoConstants.mp4HeaderPattern) != -1;
       expect(hasVideoContent, true);
     });
   });
